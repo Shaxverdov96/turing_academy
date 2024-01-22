68471c8c99779ee1cb43c9e78ca1adf4bb504e6b
Eldar199 <elikk_from_ge@mail.ru>
2024-01-22 15:42:41 +0400

    Initial commit

---
+++ src/main/java/org/Leesson1/Main.java
@@ -0,0 +1,32 @@
+package org.Leesson1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//         int a = 5;
+//         int b = 4;
+//
+//        System.out.println(a++);
+//        System.out.println(b--);
+//        System.out.println(++b);
+//        System.out.println(--a);
+//        System.out.println(12>>>1);
+//        System.out.println(14/2 == 0 ? "EVEN" : "ODD");
+//        Scanner scanner = new Scanner(System.in);
+//
+//        String input = scanner.nextLine();
+
+//        byte b1=4; //{-128;127}
+//        byte b2= (byte)129;
+//        System.out.println(b1);
+//        System.out.println(b2);
+//        int i1 = b1;
+//        System.out.println();
+        int i2 =130;
+        byte b2 = (byte)12;
+
+
+    }
+}
